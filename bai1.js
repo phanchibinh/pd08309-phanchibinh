0b8ab9a05ffd35c44c802008b427dddbf2116a18
phanchibinh <127082050+phanchibinh@users.noreply.github.com>
2023-03-13 12:33:47 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,25 @@
+// enter a, b, c
+let a = prompt("enter a : ");
+let b = prompt("enter b : ");
+let c = prompt("enter c : ");
+
+if (a == 0) {
+    // b x + c = 0
+}else {
+    let delta = Math.pow(b, 2) - 4 * a * c;
+
+    if (delta == 0) {
+        const x = -b / (2 * a);
+
+        document.writeln("<br> Phuong trinh co nghiem kep: x= ${x}");
+    }else if (delta < 0){
+        document.writeln("<br> Phuong trinh vo nghiem");
+    }else {
+        document.writeln("<br> Phuong trinh co 2 nghiem phan biet");
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        
+        document.writeln("<br> Nghiem x1 = ${x1}");
+        document.writeln("<br> Nghiem x2 = ${x2}");
+    }
+}
